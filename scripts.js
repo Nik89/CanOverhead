f6f668f18a3c2de88d6fd7f55a524283ae657cd2
Nicola Furlan <nicola_f89@hotmail.com>
2021-01-02 16:51:49 +0100

    Added structures and methods for v1 in HTML page

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let computeV1 = () => {
+    // Parse input fields
+    let payloadLengthByte = parseInt(document.getElementById("v1-in-payload-length").value);
+
+    if (payloadLengthByte < 0 || payloadLengthByte > 8) {
+        // Set error and clear existing output
+        document.getElementById("v1-input-error").innerHTML = "The value should be between 0 and 8 bytes";
+        document.getElementById("v1-out-frames-bits-min").innerHTML = "";
+        document.getElementById("v1-out-frames-bits-max").innerHTML = "";
+    } else {
+        // Clear errors
+        document.getElementById("v1-input-error").innerHTML = "";
+
+        // Calculate
+        let payloadLengthBit = payloadLengthByte * 8;
+        let payloadLengthBitMax = BitSequence.maxLengthAfterStuffing(payloadLengthBit);
+
+        // Set outputs
+        document.getElementById("v1-out-frames-bits-min").innerHTML = payloadLengthBit;
+        document.getElementById("v1-out-frames-bits-max").innerHTML = payloadLengthBitMax;
+    }
+}
