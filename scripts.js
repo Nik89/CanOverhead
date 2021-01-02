12599da3d39c12e3148609c3dc123aee8f3558e6
Nicola Furlan <nicola_f89@hotmail.com>
2021-01-02 20:11:36 +0100

    Initial files for v2

--- scripts.js
+++ scripts.js
@@ -23,3 +23,31 @@ let computeV1 = () => {
         document.getElementById("v1-out-frames-bits-max").innerHTML = payloadLengthBitMax;
     }
 }
+document.getElementById("v2-in-payload").value = "1111111111000000000011111111110000000000111111111100000000001111";
+let computeV2 = () => {
+    // Parse input fields
+    let identifierStr = document.getElementById("v2-in-identified").value;
+    let payloadStr = document.getElementById("v2-in-payload").value;
+
+    if (identifierStr !== 11) {
+        // Set error and clear existing output
+        document.getElementById("v2-input-id-error").innerHTML = "The identifier should be of 11 bits";
+        //document.getElementById("").innerHTML = "";
+        //document.getElementById("").innerHTML = "";
+    } else if (payloadStr > 64 || (payloadStr % 8 !== 0)) {
+        // Set error and clear existing output
+        document.getElementById("v2-input-pl-error").innerHTML = "Payload should be less than 64 bits and should be N bites long";
+    } else {
+        // Clear errors
+        document.getElementById("v2-input-id-error").innerHTML = "";
+        document.getElementById("v2-input-pl-error").innerHTML = "";
+
+        // Calculate
+        let payloadLengthBit = payloadLengthByte * 8;
+        let payloadLengthBitMax = BitSequence.maxLengthAfterStuffing(payloadLengthBit);
+
+        // Set outputs
+        document.getElementById("v1-out-frames-bits-min").innerHTML = payloadLengthBit;
+        document.getElementById("v1-out-frames-bits-max").innerHTML = payloadLengthBitMax;
+    }
+}
