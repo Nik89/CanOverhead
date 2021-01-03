08e5406474d5cf10b11c8a985c41e6ce8eaf26ce
Nicola Furlan <nicola_f89@hotmail.com>
2021-01-03 17:53:55 +0100

    Added deploy basic functions

--- deploy.py
+++ deploy.py
@@ -1,2 +1,19 @@
-def greet(name):
-    print("Hello " + name)
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+import shutil
+
+BUILD_DIR = "builds"
+
+
+def cleanup():
+    shutil.rmtree(BUILD_DIR, ignore_errors=True)
+    os.mkdir(BUILD_DIR)
+
+
+def deploy():
+    cleanup()
+
+
+if __name__ == "__main__":
+    deploy()
