538768a7306984429e2969b8de0515324bd4f6be
Matjaž Guštin <dev@matjaz.it>
2021-01-30 18:19:33 +0100

    Display also length of fields in bits, syntax sugar
    - CAN frame fields are now prefixed with a "[XX bits] " string that
      shows their length
    - add functions display() to write the inner HTML into a div by
      the given ID to shorten the code in other functions
    - add function clear() to delete the content of a div, similar
      as above
    - sort functions in the file a bit better (utilities on top)
    - use template string e.g. `I am ${myAge} years old` instead of
      string concatenation to improve output format clarity

--- HtmlToLibAdapter.js
+++ HtmlToLibAdapter.js
@@ -11,30 +11,27 @@
 // TODO press enter to run compute
 
 /**
- * Erases the text on the page displaying any error messages and any
- * output from the previous calculation.
+ * Writes (thus displays) the inner HTML into a div with the given ID.
+ *
+ * It's basically just syntax sugar to shorten other code lines.
+ *
+ * @param {string} id identifier of the div where to write
+ * @param {string} innerHtml HTML string to display
  */
-function clearErrorsAndOutputs() {
-    // Clear errors
-    document.getElementById("input_can_identifier_error").innerHTML = "";
-    document.getElementById("input_can_payload_error").innerHTML = "";
-    document.getElementById("input_unknown_error").innerHTML = "";
-    // Clear outputs
-    document.getElementById("output_can_whole_frame").innerHTML = "";
-    document.getElementById("output_can_whole_frame_stuffed").innerHTML = "";
-    document.getElementById("output_can_field01").innerHTML = "";
-    document.getElementById("output_can_field02").innerHTML = "";
-    document.getElementById("output_can_field03").innerHTML = "";
-    document.getElementById("output_can_field04").innerHTML = "";
-    document.getElementById("output_can_field05").innerHTML = "";
-    document.getElementById("output_can_field06").innerHTML = "";
-    document.getElementById("output_can_field07").innerHTML = "";
-    document.getElementById("output_can_field08").innerHTML = "";
-    document.getElementById("output_can_field09").innerHTML = "";
-    document.getElementById("output_can_field10").innerHTML = "";
-    document.getElementById("output_can_field11").innerHTML = "";
-    document.getElementById("output_can_field12").innerHTML = "";
-    document.getElementById("output_can_field13").innerHTML = "";
+function display(id, innerHtml) {
+    document.getElementById(id).innerHTML = innerHtml;
+}
+
+/**
+ * Erases the inner HTML of the div with the given ID, removing any
+ * previously displayed content.
+ *
+ * It's basically just syntax sugar to shorten other code lines.
+ *
+ * @param {string} id identifier of the div where to remove content
+ */
+function clear(id) {
+    display(id, "");
 }
 
 /**
@@ -42,7 +39,7 @@ function clearErrorsAndOutputs() {
  * @param {string} msg
  */
 function displayCanIdentifierError(msg) {
-    document.getElementById("input_can_identifier_error").innerHTML = msg;
+    display("input_can_identifier_error", msg);
 }
 
 /**
@@ -50,16 +47,46 @@ function displayCanIdentifierError(msg) {
  * @param {string} msg
  */
 function displayCanPayloadError(msg) {
-    document.getElementById("input_can_payload_error").innerHTML = msg;
+    display("input_can_payload_error", msg);
 }
 
 /**
  * Prints a generic error message related to an invalid input.
  * @param {string} msg
  */
-function printUnknownError(msg) {
-    document.getElementById("input_unknown_error").innerHTML = msg;
+function displayUnknownError(msg) {
+    display("input_unknown_error", msg);
 }
+
+/**
+ * Erases the text on the page displaying any error messages and any
+ * output from the previous calculation.
+ */
+function clearErrorsAndOutputs() {
+    // Clear errors
+    clear("input_can_identifier_error");
+    clear("input_can_payload_error");
+    clear("input_unknown_error");
+    // Clear outputs about the whole frame
+    clear("output_can_whole_frame");
+    clear("output_can_whole_frame_stuffed");
+    clear("output_max_length");
+    // Clear outputs about the frame fields
+    clear("output_can_field01");
+    clear("output_can_field02");
+    clear("output_can_field03");
+    clear("output_can_field04");
+    clear("output_can_field05");
+    clear("output_can_field06");
+    clear("output_can_field07");
+    clear("output_can_field08");
+    clear("output_can_field09");
+    clear("output_can_field10");
+    clear("output_can_field11");
+    clear("output_can_field12");
+    clear("output_can_field13");
+}
+
 
 /**
  * Computes and prints the whole CAN frame with 11 bit ID as bit sequence.
@@ -68,57 +95,66 @@ function printUnknownError(msg) {
 function displayCanFrame11BitWholeFrame(canFrame) {
     // Without stuffing
     let bits = canFrame.wholeFrame();
-    document.getElementById("output_can_whole_frame").innerHTML =
-        "["
-        + bits.length().toString()
-        + " bits]<br/>"
+    let frameAsString = `[${bits.length()} bits]<br/>`
         + bits.toBinStringWithSpacesLeftAlign();
+    display("output_can_whole_frame", frameAsString);
     // With stuffing
     let bitsWithStuffs = canFrame.wholeFrameStuffed();
     let stuffBitsAmount = bitsWithStuffs.length() - bits.length();
-    document.getElementById("output_can_whole_frame_stuffed").innerHTML =
-        "["
-        + bitsWithStuffs.length().toString()
-        + " bits, of which "
-        + stuffBitsAmount
-        + " stuff bits]<br/>"
+    frameAsString = `[${bitsWithStuffs.length()} bits, `
+        + `of which ${stuffBitsAmount} stuff bits]<br/>`
         + bitsWithStuffs.toBinStringWithSpacesLeftAlign();
+    display("output_can_whole_frame_stuffed", frameAsString);
     // Max theoretical length
-    document.getElementById("output_max_length").innerHTML =
-        canFrame.maxLengthAfterStuffing().toString(10) + " bits";
+    display("output_max_length", `${canFrame.maxLengthAfterStuffing()} bits`);
 }
 
 /**
- * Computes and prints all fields of a CAN frame with 11 bit ID.
+ * Computes and prints all fields of a CAN frame with 11 bit ID, along with
+ * their length in bits.
  * @param {CanFrame11Bit} canFrame
  */
 function displayCanFrame11BitFields(canFrame) {
-    document.getElementById("output_can_field01").innerHTML =
-        canFrame.field01_startOfFrame().toBinString();
-    document.getElementById("output_can_field02").innerHTML =
-        canFrame.field02_identifier().toBinStringWithSpacesRightAlign();
-    document.getElementById("output_can_field03").innerHTML =
-        canFrame.field03_remoteTransmissionRequest().toBinString();
-    document.getElementById("output_can_field04").innerHTML =
-        canFrame.field04_identifierExtensionBit().toBinString();
-    document.getElementById("output_can_field05").innerHTML =
-        canFrame.field05_reservedBit().toBinString();
-    document.getElementById("output_can_field06").innerHTML =
-        canFrame.field06_dataLengthCode().toBinString();
-    document.getElementById("output_can_field07").innerHTML =
-        canFrame.field07_dataField().toBinStringWithSpacesRightAlign();
-    document.getElementById("output_can_field08").innerHTML =
-        canFrame.field08_crc().toBinStringWithSpacesRightAlign();
-    document.getElementById("output_can_field09").innerHTML =
-        canFrame.field09_crcDelimiter().toBinString();
-    document.getElementById("output_can_field10").innerHTML =
-        canFrame.field10_ackSlot().toBinString();
-    document.getElementById("output_can_field11").innerHTML =
-        canFrame.field11_ackDelimiter().toBinString();
-    document.getElementById("output_can_field12").innerHTML =
-        canFrame.field12_endOfFrame().toBinString();
-    document.getElementById("output_can_field13").innerHTML =
-        canFrame.field13_pauseAfterFrame().toBinString();
+    let field;
+    field = canFrame.field01_startOfFrame();
+    display("output_can_field01",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field02_identifier();
+    display("output_can_field02",
+        `[${field.length()} bits] ${field.toBinStringWithSpacesRightAlign()}`);
+    field = canFrame.field03_remoteTransmissionRequest();
+    display("output_can_field03",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field04_identifierExtensionBit();
+    display("output_can_field04",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field05_reservedBit();
+    display("output_can_field05",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field06_dataLengthCode();
+    display("output_can_field06",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field07_dataField();
+    display("output_can_field07",
+        `[${field.length()} bits] ${field.toBinStringWithSpacesRightAlign()}`);
+    field = canFrame.field08_crc();
+    display("output_can_field08",
+        `[${field.length()} bits] ${field.toBinStringWithSpacesRightAlign()}`);
+    field = canFrame.field09_crcDelimiter();
+    display("output_can_field09",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field10_ackSlot();
+    display("output_can_field10",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field11_ackDelimiter();
+    display("output_can_field11",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field12_endOfFrame();
+    display("output_can_field12",
+        `[${field.length()} bits] ${field.toBinString()}`);
+    field = canFrame.field13_pauseAfterFrame();
+    display("output_can_field13",
+        `[${field.length()} bits] ${field.toBinString()}`);
 }
 
 /**
@@ -162,10 +198,10 @@ function calculate() {
     const identifier = parseCanIdentifierFromInputForm();
     if (identifier === null) {
         displayCanIdentifierError(
-            "Incorrect identifier format. " +
-            "The input is in base 10 by default. " +
-            "For base 16, use the '0x' prefix; " +
-            "for base 2 use the '0b' prefix.");
+            "Incorrect identifier format. "
+            + "The input is in base 10 by default. "
+            + "For base 16, use the '0x' prefix; "
+            + "for base 2 use the '0b' prefix.");
         return; // Early exit
     }
     const payload = parseCanPayloadFromInputForm();
@@ -191,7 +227,7 @@ function calculate() {
             displayCanPayloadError(err.message);
         } else {
             // Other errors, but they "should never happen".
-            printUnknownError(
+            displayUnknownError(
                 "An unexpected error occurred :( Please " +
                 "<a href=\"https://github.com/Nik89/CanOverhead/issues\">" +
                 "report</a> the conditions leading to your bug!");
